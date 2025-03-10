53234d48bceb1bcb19b24c3dfe607a27faabff5c
fatima-ezzahraalouane <falouane38@gmail.com>
2025-03-10 02:11:26 +0000

    PI-5 ajouter les routes pour parking

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ParkingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +19,39 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    // return $request->user();
+
+    Route::get('/parkings', [ParkingController::class, 'index']);
+    Route::post('/parkings', [ParkingController::class, 'store']);
+    Route::get('/parkings/{id}', [ParkingController::class, 'show']);
+    Route::put('/parkings/{id}', [ParkingController::class, 'update']);
+    Route::delete('/parkings/{id}', [ParkingController::class, 'destroy']);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+// Route::post('/register', [AuthController::class, 'register']);
+// Route::post('/login', [AuthController::class, 'login']);
+
+// Route::middleware('auth:sanctum')->group(function () {
+//     Route::get('/parkings', [ParkingController::class, 'index']);
+//     Route::post('/parkings', [ParkingController::class, 'store']);
+//     Route::get('/parkings/{id}', [ParkingController::class, 'show']);
+//     Route::put('/parkings/{id}', [ParkingController::class, 'update']);
+//     Route::delete('/parkings/{id}', [ParkingController::class, 'destroy']);
+
+//     Route::get('/reservations', [ReservationController::class, 'index']);
+//     Route::post('/reservations', [ReservationController::class, 'store']);
+//     Route::get('/reservations/{id}', [ReservationController::class, 'show']);
+//     Route::put('/reservations/{id}', [ReservationController::class, 'update']);
+//     Route::delete('/reservations/{id}', [ReservationController::class, 'destroy']);
+// });
