c317e15ea8f9ed552e9984f641364073a3e9f5c4
fatima-ezzahraalouane <falouane38@gmail.com>
2025-03-11 13:13:23 +0000

    PI-6 creer methode stats() pour compte nombre total des parkings, des reservations et calcule le nombre total de places disponibles

---
+++ app/Http/Controllers/AdminStatsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Parking;
+use App\Models\Reservation;
+
+class AdminStatsController extends Controller
+{
+    public function stats()
+    {
+        $totalParkings = Parking::count();
+        $totalReservations = Reservation::count();
+        $totalAvailableSpots = Parking::sum('available_spots');
+    }
+}
