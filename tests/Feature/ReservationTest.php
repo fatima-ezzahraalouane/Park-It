37ae9ecb33414a12a1aab145ac47b38fd69a2556
fatima-ezzahraalouane <falouane38@gmail.com>
2025-03-11 23:12:13 +0000

    PI-15 le test est bien , user peut faire une reservation

--- tests/Feature/ReservationTest.php
+++ tests/Feature/ReservationTest.php
@@ -18,7 +18,7 @@ class ReservationTest extends TestCase
         $user = User::factory()->create();
         $parking = Parking::factory()->create(['available_spots' => 5]);
 
-        $response = $this->actingAs($user, 'sunctum')->postJson('/api/reservations', [
+        $response = $this->actingAs($user, 'sanctum')->postJson('/api/reservations', [
             'parking_id' => $parking->id,
             'start_time' => now()->addHours(1),
             'end_time' => now()->addHours(3)
@@ -27,13 +27,4 @@ class ReservationTest extends TestCase
         $response->assertStatus(201)
                 ->assertJsonStructure(['id', 'status']);
     }
-    /**
-     * A basic feature test example.
-     */
-    // public function test_example(): void
-    // {
-    //     $response = $this->get('/');
-
-    //     $response->assertStatus(200);
-    // }
 }
