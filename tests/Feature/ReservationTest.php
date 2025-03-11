2f1a080efb1479f7884e93543a702fa744a1eb30
fatima-ezzahraalouane <falouane38@gmail.com>
2025-03-11 23:49:04 +0000

    PI-15 creer test pour creer un reservation

--- tests/Feature/ReservationTest.php
+++ tests/Feature/ReservationTest.php
@@ -13,18 +13,19 @@ class ReservationTest extends TestCase
 {
     use RefreshDatabase;
 
+    // test pour creer une reservation
     public function test_user_can_make_a_reservation()
-    {
-        $user = User::factory()->create();
-        $parking = Parking::factory()->create(['available_spots' => 5]);
+{
+    $user = User::factory()->create();
+    $parking = Parking::factory()->create(['available_spots' => 5]);
 
-        $response = $this->actingAs($user, 'sanctum')->postJson('/api/reservations', [
-            'parking_id' => $parking->id,
-            'start_time' => now()->addHours(1),
-            'end_time' => now()->addHours(3)
-        ]);
+    $response = $this->actingAs($user, 'sanctum')->postJson('/api/reservations', [
+        'parking_id' => $parking->id,
+        'start_time' => now()->addHours(1),
+        'end_time' => now()->addHours(3)
+    ]);
 
-        $response->assertStatus(201)
-                ->assertJsonStructure(['id', 'status']);
-    }
+    $response->assertStatus(201)
+            ->assertJsonStructure(['id', 'status']);
+}
 }
