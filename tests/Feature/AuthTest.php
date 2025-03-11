df5180aa5e367a967b30eee1018dd6b58ac0ff97
fatima-ezzahraalouane <falouane38@gmail.com>
2025-03-11 22:40:56 +0000

    PI-15 creer methode pour tester register

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -5,16 +5,32 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class AuthTest extends TestCase
 {
+    use RefreshDatabase;
+
+    public function test_user_can_register()
+    {
+        $response = $this->postJson('/api/register', [
+            'name' => 'Test User',
+            'email' => 'test@gmail.com',
+            'password' => 'password',
+            'password_confirmation' => 'password'
+        ]);
+
+        $response->assertStatus(201);
+    }
+
+    
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    // public function test_example(): void
+    // {
+    //     $response = $this->get('/');
 
-        $response->assertStatus(200);
-    }
+    //     $response->assertStatus(200);
+    // }
 }
