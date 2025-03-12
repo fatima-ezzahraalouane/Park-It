ce0c4546a3c225442a76f385ee4b05d4d16df07d
fatima-ezzahraalouane <falouane38@gmail.com>
2025-03-12 02:16:25 +0000

    PI-17 ajouter schema dans cotroller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,10 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *     title="Park' It API",
+ *     version="1.0",
+ *     description="Documentation de l'API de gestion des parkings et réservations",
+ *     @OA\Contact(
+ *         email="support@parkit.com"
+ *     )
+ * )
+ * 
+ * @OA\SecurityScheme(
+ *     securityScheme="sanctum",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT"
+ * )
+ * 
+ *
+ */
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
