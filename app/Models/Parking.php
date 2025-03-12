f643d6cfa6dc55cea9bd79eb9a92ddb46e4c0701
fatima-ezzahraalouane <falouane38@gmail.com>
2025-03-12 02:17:55 +0000

    PI-17 ajouter schema dans model

--- app/Models/Parking.php
+++ app/Models/Parking.php
@@ -4,7 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Schema(
+ *     schema="Parking",
+ *     description="Modèle de Parking",
+ *     @OA\Property(property="id", type="integer", example=1),
+ *     @OA\Property(property="name", type="string", example="Parking Hassan II"),
+ *     @OA\Property(property="location", type="string", example="Rue Hassan II, Marrakech"),
+ *     @OA\Property(property="total_spots", type="integer", example=50),
+ *     @OA\Property(property="available_spots", type="integer", example=45)
+ * )
+ */
 class Parking extends Model
 {
     use HasFactory;
